f1d9d925a2986da8b43cae16daebfe75ceb66b25
MarcoBPBakker <132364953+MarcoBPBakker@users.noreply.github.com>
2023-05-02 15:28:21 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,3 +9,6 @@ streamlit.text('🐔 gekookte kip op kattengras')
 streamlit.text('🥣 haring gemarineerd in zalmsaus')
 
 streamlit.header('🍌🥭 Smoothie\'s voor de baasjes? 🥝🍇')
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
