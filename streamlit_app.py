b1ba06720c967bd16ec6a0878743076a9dfe0a3d
MarcoBPBakker <132364953+MarcoBPBakker@users.noreply.github.com>
2023-05-02 15:23:40 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('Pablo & Pedro\'s Kattenbak')
+
+streamlit.header('Ontbijt Menu')
+streamlit.text('brokjes met kattemelk')
+streamlit.text('gekookte kip op kattengras')
+streamlit.text('haring gemarineerd in zalmsaus')
